fbb09e58b081d8dc2d6e65b1533e322bea0b9837
ginlord111 <nicandro.ronnie@gmail.com>
2024-04-01 23:13:05 +0800

    updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,48 +1,54 @@
-'use client'
+"use client";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
-import { SignIn, SignInButton, UserButton, useOrganization, useUser } from "@clerk/nextjs";
+import {
+  SignIn,
+  SignInButton,
+  UserButton,
+  useOrganization,
+  useUser,
+} from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import Image from "next/image";
 import { hasAccessToOrg } from "@/convex/files";
 export default function Home() {
-  const organization = useOrganization()
-  const user = useUser()  
-  let orgId:string | undefined = undefined;
+  const organization = useOrganization();
+  const user = useUser();
+  let orgId: string | undefined = undefined;
 
-    if(organization.isLoaded &&  user.isLoaded){
-   orgId = organization.organization?.id ?? user.user?.id
-    }
-  const files = useQuery(api.files.getFiles, orgId ? {orgId}: 'skip');
-  const createFile = useMutation(api.files.createFile)
- return (
-<> 
-  <MaxWidthWrapper className="mt-10">
-   <SignInButton>
-    <Button>
-      Sign in
-    </Button>
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
+  const createFile = useMutation(api.files.createFile);
+  return (
+    <>
+      <MaxWidthWrapper className="mt-10">
+        <SignInButton>
+          <Button>Sign in</Button>
+        </SignInButton>
 
-   </SignInButton>
-
-   <UserButton />
-      <Button onClick={()=>{
-        if(!orgId){
-          return ;
-        }
-        createFile({
-        name:"hello world",
-        orgId,
-      })}}>
-        Click Me
-      </Button>
-      {files?.map((file) => (
-        <div key={file._id} className="text-black">
-          {file.name}
-        </div>
-      ))}
-  </MaxWidthWrapper>
-  </>
+        <UserButton />
+        <Button
+          onClick={() => {
+            if (!orgId) {
+              return;
+            }
+            createFile({
+              name: "hello world",
+              orgId,
+            });
+          }}
+        >
+          Click Me
+        </Button>
+        {files?.map((file) => (
+          <div key={file._id} className="text-black">
+            {file.name}
+          </div>
+        ))}
+      </MaxWidthWrapper>
+    </>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
-
-
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-  publicRoutes:['/']
+  publicRoutes: ["/"],
 });
- 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
