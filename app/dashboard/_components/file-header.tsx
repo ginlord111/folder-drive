58b9ae15dce261c1fb44597b766cbba03771457f
ginlord111 <nicandro.ronnie@gmail.com>
2024-04-04 14:50:23 +0800

    kekw

--- app/dashboard/_components/file-header.tsx
+++ app/dashboard/_components/file-header.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
+"use client";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FileSearch } from "lucide-react";
+import UploadFile from "@/components/UploadFile";
 const FileHeader = () => {
   return (
-    <div className="flex items-center justify-between px-10">
-    <div className="text-4xl font-bold tracking-tight">
-      <h1>Your Files</h1>
+    <div className="flex items-center justify-between ">
+      <div className="md:text-4xl text-xl font-bold tracking-tight whitespace-nowrap">
+        <h1>Your Files</h1>
+      </div>
+      <div className="flex items-center gap-3 flex-shrink-1">
+        <Input type="text" placeholder="Search file" />
+        <Button variant="default">
+          <FileSearch className="mr-2" />
+          Search
+        </Button>
+      </div>
+      <div>
+        <UploadFile />
+      </div>
     </div>
-    <div className="flex items-center gap-3">
-      <Input type="text" placeholder="Search file" />
-      <Button variant="default" className="">
-        <FileSearch className="mr-2" />
-        Search
-      </Button>
-    </div>
-    <div>
-      <Button>Upload File</Button>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default FileHeader
+export default FileHeader;
--- app/dashboard/files/page.tsx
+++ app/dashboard/files/page.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import FileHeader from "../_components/file-header";
-
-const page = () => {
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { LayoutGrid, Rows2 } from "lucide-react";
+const FilesPage = () => {
   return (
-    <div className="relative bg-white ">
+    <div className="relative bg-white px-10">
       <FileHeader />
+      <div className="relative mt-10">
+        <div className="flex justify-between py-2">
+          <div>
+            <Tabs defaultValue="account" className="w-[200px]">
+              <TabsList className="grid w-full grid-cols-2 h-[50px] py-2">
+                <TabsTrigger value="account">
+                  <p className="mr-2">Grid</p> <LayoutGrid width={20} />
+                </TabsTrigger>
+                <TabsTrigger value="password">
+                  <p className="mr-2">Table</p> <Rows2 width={20} />
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default page;
+export default FilesPage;
