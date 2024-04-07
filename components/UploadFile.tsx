b8a77b0ae50907251ddf669cedf7580fbc7c8b54
ginlord <nicandro.ronnie@gmail.com>
2024-04-07 19:47:42 +0800

    fix

--- components/UploadFile.tsx
+++ components/UploadFile.tsx
@@ -9,40 +9,93 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-const UploadFileCard = () => (
-  <Dialog>
-    <DialogTrigger>
-      <Button>Upload File</Button>
-    </DialogTrigger>
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle className="pb-5">Upload your file here</DialogTitle>
-        <DialogDescription>
-          This will be accessible by anyone in your organization
-        </DialogDescription>
-      </DialogHeader>
-      <div className="flex flex-col items-start gap-5 w-full">
-        <div className="pb-3 w-full">
-          <Label htmlFor="title">Title</Label>
-          <Input
-            type="text"
-            placeholder="Input your folder title"
-            className="w-full mt-3"
-          />
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+const formSchema = z.object({
+  title:z.string().min(5),
+  file: z.string(),
+});
+const UploadFileCard = () => {
+  const onSubmit = (values: z.infer<typeof formSchema>) => {};
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file:'',
+    },
+  });
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button>Upload File</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="pb-5">Upload your file here</DialogTitle>
+          <DialogDescription>
+            This will be accessible by anyone in your organization
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col items-start gap-5 w-full">
+          <div className="pb-3 w-full">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Input your file title here"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="file"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>File</FormLabel>
+                      <FormControl>
+                        <Input
+                        type="file"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="w-full">Submit</Button>
+              </form>
+            </Form>
+          </div>
         </div>
-        <div className="pt-4 w-full">
-          <Label htmlFor="file">File</Label>
-          <Input type="file" className="w-full mt-3" />
-        </div>
-        <div className="flex items-center w-full justify-center pt-5">
-          <Button className="w-full">Submit</Button>
-        </div>
-      </div>
-    </DialogContent>
-  </Dialog>
-);
+      </DialogContent>
+    </Dialog>
+  );
+};
 const UploadFile = () => {
   return (
     <div className="relative flex items-center">
