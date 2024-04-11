b1e2349dfe12abd668e2658ee41847f74591449b
ginlord <nicandro.ronnie@gmail.com>
2024-04-12 00:23:24 +0800

    updated

--- components/SkeletonFile.tsx
+++ components/SkeletonFile.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 const SkeletonFile = () => {
   return (
-    <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 mt-[50px]">
-      <Skeleton className="md:w-[500px] md:h-[360px] sm:w-[300px] sm:h-[160px] bg-zinc-200" />
-      <Skeleton className="md:w-[500px] md:h-[360px] sm:w-[300px] sm:h-[160px] bg-zinc-200" />
-      <Skeleton className="md:w-[500px] md:h-[360px] sm:w-[300px] sm:h-[160px] bg-zinc-200" />
+    <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 mt-[50px] lg:gap-0 gap-3">
+      <Skeleton className="lg:w-[500px] lg:h-[360px] sm:w-[150px] sm:h-[160px] bg-zinc-200 w-[200px] h-[150px]" />
+      <Skeleton className="lg:w-[500px] lg:h-[360px] sm:w-[150px] sm:h-[160px] bg-zinc-200 w-[200px] h-[150px]" />
+      <Skeleton className="lg:w-[500px] lg:h-[360px] sm:w-[150px] sm:h-[160px] bg-zinc-200 w-[200px] h-[150px]" />
     </div>
   );
 };
